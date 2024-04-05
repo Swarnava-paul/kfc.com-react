e6483366c5702fdabe1a069a153af2cf5eae6e52
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-05 17:25:35 +0530

    redline loading is successfully omplemented

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 
 // components
 import { Navbar } from './components/navbar'
+import { Redlineloading } from './components/loading'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
     <Navbar/>
+    <Redlineloading/>
     </>
   )
 }
--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,11 +1,27 @@
-import { Box } from "@chakra-ui/react"
+import { Box} from "@chakra-ui/react"
+import { useState,useEffect} from "react"
 
+import '../App.css'
 export const Redlineloading = ()=>{
+
+    const[displayloading,setDisplayLoading]=useState('grid')
+    const[translate,setTranslate]=useState('default-translate-loading-line')
+   
+ useEffect(()=>{
+ setTimeout(()=>{
+   setTranslate('on-translate-loadingg-line')
+ },200)
+ setTimeout(()=>{
+ setDisplayLoading('none')
+ },3000)
+ },[])
 
     return(
         <>
-        <Box>
-            
+        <Box bg='white' className={translate} position='absolute' display={displayloading} w='100%'  h={{base:'150vh',sm:'170vh',md:"190vh",lg:"240vh"}} gridTemplateColumns={{base:'repeat(3,9%)',sm:'repeat(3,6%)',md:'repeat(3,5%)',lg:'repeat(3,4%)'}} justifyContent='center'  columnGap={5}>
+            <Box bg='red' h='100%'></Box>
+            <Box bg='red' h='60%'></Box>
+            <Box bg='red' h='30%'></Box>
         </Box>
         </>
     )
