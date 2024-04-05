4639d351ecbae8a3760f37cf25d63678324e1256
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-05 18:13:41 +0530

    main image creation process started

---
+++ src/components/mainimage.jsx
@@ -0,0 +1,14 @@
+import { Image } from "@chakra-ui/react";
+
+export const Imagemain =() =>{
+
+    const image_array=[
+        
+    ]
+
+    return(
+        <>
+        <Image/>
+        </>
+    )
+}
