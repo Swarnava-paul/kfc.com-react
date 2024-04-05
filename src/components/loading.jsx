b378db0d6113ba82f953e996fb843a3293b0f0aa
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-05 22:53:59 +0530

    now readline vertical loading are optimized in  terms of size

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -2,6 +2,7 @@ import { Box} from "@chakra-ui/react"
 import { useState,useEffect} from "react"
 
 import '../App.css'
+import { transform } from "framer-motion"
 export const Redlineloading = ()=>{
 
     const[displayloading,setDisplayLoading]=useState('grid')
@@ -18,7 +19,7 @@ export const Redlineloading = ()=>{
 
     return(
         <>
-        <Box bg='white' className={translate} position='absolute' display={displayloading} w='100%'  h={{base:'150vh',sm:'170vh',md:"190vh",lg:"240vh"}} gridTemplateColumns={{base:'repeat(3,9%)',sm:'repeat(3,6%)',md:'repeat(3,5%)',lg:'repeat(3,5%)'}} justifyContent='center'  columnGap={10}>
+        <Box bg='white' className={translate}  position='absolute' display={displayloading} w='100%'  h={{base:'150vh',sm:'170vh',md:"190vh",lg:"240vh"}} gridTemplateColumns={{base:'repeat(3,12%)',sm:'repeat(3,6%)',md:'repeat(3,5%)',lg:'repeat(3,5%)'}} justifyContent='center'  columnGap={{base:'5',sm:'6',md:'8',lg:'10'}}>
             <Box bg='red' h='100%'></Box>
             <Box bg='red' h='60%'></Box>
             <Box bg='red' h='30%'></Box>
