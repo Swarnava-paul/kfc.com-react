a2f9c066e3c0fc3d736d6c763e5dcfe87a8706ee
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-06 10:55:18 +0530

    first section of categories div completed Browse categories text inserted and also a vertical row by using a Box

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,13 +1,19 @@
 
-import { Box ,Grid,Text,Image } from "@chakra-ui/react";
+import { Box ,Grid,Text,Image ,Flex , Divider} from "@chakra-ui/react";
 
 export const Homapage = ()=>{
 
     return(
         <>
-        <Grid>
-            <Box></Box>
+        <Grid placeItems='center' mt='12%' border='1px' borderColor='red' templateRows='10% 90%' h='150vh'>
+
+            <Flex  align='center' pl='8' w='80%'>
+                <Text fontSize={30} fontWeight='700' color='RGB(32, 33, 36)'>BROWSE CATEGORIES</Text>   
+                <Box w='60%' border='1px' borderColor='grey'></Box>
+            </Flex>
+
             <Grid></Grid>
+
         </Grid>
         </>
     )
