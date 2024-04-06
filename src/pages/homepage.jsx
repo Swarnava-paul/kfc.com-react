d0fdf425f8f0c3b00eaf986daad4371262f706e7
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-06 15:43:16 +0530

    responsiveness added for food images with categoties section

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -50,14 +50,14 @@ export const Homapage = ()=>{
             </Flex>{/**end of Flex that contains Text and one box the box is use for mimeake a vertical row  */}
 
 
-            <Grid mt='9vh' h='120vh' templateColumns='repeat(4,18%)' templateRows='repeat(2,34%)' justifyContent='center' columnGap='6' rowGap='7'>
+            <Grid mt='9vh' h='120vh' templateColumns={{base:'repeat(2,40%)',sm:'repeat(2,41%)',md:'repeat(3,30%)',lg:'repeat(4,18%)'}} templateRows={{base:'repeat(4,20%)',sm:'repeat(2,29%)',md:'repeat(2,25%)',lg:'repeat(2,34%)'}} justifyContent='center' columnGap='6' rowGap='7'>
                 
               {
                 categories_array.map(i=>(
 
                 <Grid key={i.id} bg='RGB(248, 247, 245)' placeItems='center'>
                     <Image  h='100%' src={i.img}/>
-                    <Button fontWeight='800' fontSize={15} bg='RGB(248, 247, 245)' h='100%' w='100%' borderBottomLeftRadius='none' borderBottomRightRadius='none' gap={3}>{i.text}</Button>
+                    <Button fontWeight='800' fontSize={{base:'10px',sm:'15px',md:'15px',lg:'16px'}} bg='RGB(248, 247, 245)' h='100%' w='100%' borderBottomLeftRadius='none' borderBottomRightRadius='none' gap={3}>{i.text}</Button>
                 </Grid>
 
                 ))
