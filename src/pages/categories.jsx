e45150f923cb4c46e646d898a58e9c0187dfd1f5
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-09 14:01:50 +0530

    sidebar and main content holder created for categories page

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -1,14 +1,22 @@
-import {Box,Grid,Text,Button,Image} from '@chakra-ui/react'
+import {Box,Grid,Text,Button,Image, border} from '@chakra-ui/react'
 
 export const Categoriespage = ()=>{
-
+    
+let border ={
+    border:'2px',
+    borderColor:"red",
+}
+let border2 ={
+    border:'2px',
+    borderColor:"black",
+}
     return(
         <>
-        <Grid>
+        <Grid >
 
-        <Grid></Grid>{/**side bar */}
+        <Grid sx={border} h='90vh' pos='fixed' w='20%'></Grid>{/**side bar */}
         
-         <Box></Box>{/** main content holder */}
+         <Box sx={border2} h='350vh' pl='20%' w='100%'></Box>{/** main content holder */}
 
         </Grid>{/**Main holder */}
         </>
