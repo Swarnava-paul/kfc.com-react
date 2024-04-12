8f56b4141c216252386df5bfeb1828cbbccbbc7b
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-12 17:50:32 +0530

    veg component created

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -107,3 +107,14 @@ const Nonveg= ()=>{
     )
 }
 
+const Veg = ()=>{
+    
+    return(
+        <>
+        <Flex  justify='space-evenly' w={{base:'50%',sm:'40%',md:'44%',lg:"30%"}} align='center' ml='1'>
+         <Image src='https://online.kfc.co.in/static/media/Veg_dot_Icon.d1a3902d.svg'/>
+         <Text fontSize={12} fontWeight='400' color='RGB(73, 73, 73)'>Veg</Text>
+        </Flex>
+        </>
+    )
+}
