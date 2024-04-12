bf4ae4f78c3b647ead4948a151c79209b4e18534
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-12 18:02:31 +0530

    non veg component updated

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -101,7 +101,7 @@ const Nonveg= ()=>{
 
     return(
         <>
-        <Flex  justify='space-evenly' w={{base:'50%',sm:'40%',md:'44%',lg:"30%"}} align='center' ml='1'>
+        <Flex  justify='space-evenly' w={{base:'30%',sm:'52%',md:'52%',lg:"30%"}} align='center' ml='1'>
          <Image src='https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg'/>
          <Text fontSize={12} fontWeight='400' color='RGB(73, 73, 73)'>Non Veg</Text>
         </Flex>
