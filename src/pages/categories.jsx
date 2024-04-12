f3c1eeb76c68dd0826227684fdfb0dd32ae93d11
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-12 13:07:22 +0530

    key assaign updated

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -75,7 +75,7 @@ const text_style={
             {
                 i.map(index=>(
                     
-                <Grid key={i} h='auto' rowGap={3} boxShadow='rgba(0, 0, 0, 0.24) 0px 3px 8px' pb={4} borderRadius={10}>
+                <Grid key={index.price+Math.random()} h='auto' rowGap={3} boxShadow='rgba(0, 0, 0, 0.24) 0px 3px 8px' pb={4} borderRadius={10}>
                 <Image width={{base:'100%',sm:"100%",md:'100%',lg:'100%'}} borderTopLeftRadius={10} borderTopRightRadius={10} src={index.img}/>
                 <Box  w='93%'><Text ml='3' fontSize={16} fontWeight='600' color='RGB(32, 33, 36)'>{index.title}</Text></Box>
                 <Nonveg/>
