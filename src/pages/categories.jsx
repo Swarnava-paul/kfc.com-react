c31a8aec7a651afac256bbb134f469c26414e58e
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-12 12:49:47 +0530

    api data mapped and ui created successfully for categories page food containers

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -65,20 +65,22 @@ const text_style={
         
          <Grid  h='100vh' overflowY='scroll' pb='10%' rowGap={28}  pt={{base:'10px',sm:'10px',md:'14px',lg:'100px'}} >
 
-            <Box h='auto' >{/**will be map */}
-              <Text ml='7px' fontSize={{base:'15px',sm:'15px',md:'20px',lg:'25px'}} fontWeight='800'>VALUE SNACKERS</Text>
+         { foodData.map(i=>(
+            
+            <Box key={Math.random()} h='auto' >{/**will be map */}
+              <Text ml='7px' fontSize={{base:'15px',sm:'15px',md:'20px',lg:'25px'}} fontWeight='800'>{i[0].head}</Text>
 
               <Grid justifyContent='center' mt='14'  templateColumns={{base:'repeat(1,70%)',sm:'repeat(3,30%)',md:'repeat(3,30%)',lg:'repeat(3,32%)'}} pb='9' columnGap={{base:"9px",sm:'15px',md:'15px',lg:'10px'}} rowGap={14}>
             
             {
-                side_bar_menu_texts.map(i=>(
+                i.map(index=>(
                     
                 <Grid key={i} h='auto' rowGap={3} boxShadow='rgba(0, 0, 0, 0.24) 0px 3px 8px' pb={4} borderRadius={10}>
-                <Image width={{base:'100%',sm:"100%",md:'100%',lg:'100%'}} borderTopLeftRadius={10} borderTopRightRadius={10} src='https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002167.jpg?ver=41.8'/>
-                <Text ml='3' fontSize={16} fontWeight='600' color='RGB(32, 33, 36)'>All Chicken Box</Text>
+                <Image width={{base:'100%',sm:"100%",md:'100%',lg:'100%'}} borderTopLeftRadius={10} borderTopRightRadius={10} src={index.img}/>
+                <Box  w='93%'><Text ml='3' fontSize={16} fontWeight='600' color='RGB(32, 33, 36)'>{index.title}</Text></Box>
                 <Nonveg/>
-                <Text ml='3' color='RGB(32, 33, 36)' fontSize={16} fontWeight='600'>₹859.05</Text>
-                 <Flex fontWeight='400' w='90%' margin='auto' p='15px 15px 15px 0px' fontSize={14} color='RGB(73, 73, 73)'>Family Deal of 3 chicken zingers, med popcorn, med fries & 1 Pepsi 475ml</Flex>
+                <Text ml='3' color='RGB(32, 33, 36)' fontSize={16} fontWeight='600'>₹{index.price}</Text>
+                 <Flex fontWeight='400' w='90%' margin='auto' p='15px 15px 15px 0px' fontSize={14} color='RGB(73, 73, 73)'>{index.description}</Flex>
                  <Flex  w='100%' justify='center'>
                  <Button pos='static' color='white' gap={2} bg='RGB(228, 0, 43)' w={{base:"90%",sm:"80%",md:'70%',lg:'70%'}} borderRadius={19} border='none' _hover={{bg:'RGB(228, 0, 43)'}}> 
                    Add to Cart
@@ -92,6 +94,9 @@ const text_style={
              </Grid>{/* Products cards Grid*/}
 
               </Box>
+         ))   
+              
+              } {/**main map */}
 
         </Grid>{/** main content holder */}
 
