4dd02e51b0e78d16b7953fb9d80de44a065b3b57
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-13 12:59:42 +0530

    button of lastsection sign me up! fontSIze updated

--- src/components/lastsection.jsx
+++ src/components/lastsection.jsx
@@ -10,7 +10,7 @@ export function Lastsection(){
             <Grid placeItems='center' mt={{base:"15px",sm:"35px",md:"50px",lg:'5px'}}>
                  <Box w={{base:'80%',sm:"50%",md:"70%",lg:"80%"}}><Text fontSize={{base:"24px",sm:'22px',md:'30px',lg:"52px"}} fontWeight='700' color='RGB(32, 33, 36)'>UNLOCK MORE FINGER LICKIN' GOOD BENEFITS</Text>
                  <Box w={{base:"98%",sm:"80%",md:'80%',lg:"60%"}}><Text fontSize={14} fontWeight='400' color='RGB(73, 73, 73)'>Create an account to get access to great deals and faster checkout.</Text></Box>
-                 <Button w={{base:"45%",sm:"47%",md:"30%",lg:"20%"}} mt={6} bg='RGB(32, 33, 36)' color='white' _hover={{bg:'RGB(32, 33, 36)'}}>Sign me up!</Button>
+                 <Button fontSize={{base:"12px",sm:'12px',md:"13px",lg:'15px'}} w={{base:"45%",sm:"47%",md:"30%",lg:"20%"}} mt={6} bg='RGB(32, 33, 36)' color='white' _hover={{bg:'RGB(32, 33, 36)'}}>Sign me up!</Button>
                  </Box>               
            </Grid>
            
