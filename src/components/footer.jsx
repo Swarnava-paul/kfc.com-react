b2a5f8b06253ef3a84d4cc593d603fed3b8ab76d
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-14 18:46:56 +0530

    now footer section is responsive for all screens and updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,26 @@
-import {Grid,Text,Box} from "@chakra-ui/react"
-
+import {Grid,Text,Box,Button} from "@chakra-ui/react"
+import { useState } from "react"
 
 export function Footer(){
 
+    const footer=[
+     {btnTxt:'KFC FOOD',t1:'Menu',t2:'Full Nutrition Guide',t3:'Nutrition Calculator',t4:'Food Allergies & Sensitivities'},
+     {btnTxt:'Company',t1:'About KFC',t2:'How We Make Chicken',t3:'KFC Foundation',t4:'Company Responsibilities'},
+     {btnTxt:'Careers',t1:'Resturants Careers',t2:'Corporate Careers',t3:'Culture',t4:'Growth'},
+     {btnTxt:'Resources',t1:'FAQs',t2:'Contact Us',t3:'KFC App',t4:''},
+     {btnTxt:'Find a KFC',t1:'Find A KFC',t2:'',t3:'',t4:''},
+    ]
 
     return(
         <>
-        <Grid  bg='RGB(32, 33, 36)' placeItems='center'>
-          <Firstsectiontext/>
-          <Secondsection/>
+        <Grid  bg='RGB(32, 33, 36)'  h='auto' pt={8}>
+        <Firstsectiontext/>
+        <Secondsection/>
+         {
+            footer.map(i=>(
+                <Responsivefooter key={i.btnTxt} i={i}/>
+            ))
+         }
         </Grid>
         </>
     )
@@ -20,7 +32,7 @@ const Firstsectiontext = ()=>{
 
     return (
         <>
-        <Grid   h='auto' color='white' fontSize={{base:"7px",sm:'8px',md:'9px',lg:"12px"}} fontWeight='400' rowGap={12} mt={{base:"90px",sm:"50px",md:"80px"}} justifyContent='center'>
+        <Grid display={['none','none','none','grid']}  h='auto' color='white' fontSize={{base:"7px",sm:'8px',md:'9px',lg:"12px"}} fontWeight='400' rowGap={12} mt={{base:"90px",sm:"50px",md:"80px"}} justifyContent='center'>
            <Box><Text>Calorie Statement</Text></Box>
            <Box w={{base:"90%",sm:'90%',md:"90%",lg:"100%"}}><Text>2,000 calories a day is used for general nutrition advice, but calorie needs vary. Additional nutrition information available upon request in-store and on kfc.com. Prices, participation, and product availability may vary.</Text></Box>
            <Box w={{base:"90%",sm:'90%',md:"80%",lg:"60%"}}><Text>Pepsi, Pepsi Globe, Mtn Dew, Mtn Dew Sweet Lightning, Starry are registered trademarks of PepsiCo, Inc.
@@ -42,7 +54,7 @@ const Secondsection = ()=>{
     }
     return(
         <>
-        <Grid  p='20px' rowGap={['36px','35px','33px','0px']} columnGap={['52px','59px','63px','12px']} justifyContent={['center','center','center','space-evenly']} p='30px' fontSize={12} mt={10} w='90%' templateColumns={['repeat(2,33%)','repeat(2,36%)','repeat(3,22%)','repeat(7,19%)']} color='white' >
+        <Grid display={['none','none','none','grid']} p='20px' rowGap={['36px','35px','33px','0px']} columnGap={['52px','59px','63px','12px']} justifyContent={['center','center','center','space-evenly']} p='30px' fontSize={12} mt={10} w='90%' templateColumns={['repeat(2,33%)','repeat(2,36%)','repeat(3,22%)','repeat(7,19%)']} color='white' >
             <Grid sx={child_grid_gap}>
                 <Text sx={text_style}>KFC Food</Text>
                 <Grid sx={child_grid_gap} >
@@ -89,3 +101,39 @@ const Secondsection = ()=>{
     )
 }
 
+const Responsivefooter =({i})=>{
+
+
+    const[display,setDisplay]=useState('hide')
+
+    return(
+        <>
+        <Grid display={['grid','grid','grid','none']} h='auto' color='white' rowGap={4} p={8}>
+          {
+            (display=='hide'?(
+                <Button pos='static' w='100%' _hover={{bg:'RGB(32, 33, 36)'}}  bg='RGB(32, 33, 36)' color='white' justifyContent='space-between' onClick={()=>{
+                    setDisplay('flex')
+                }}>{i.btnTxt}
+                <i className="fa-solid fa-angle-down"></i>
+                </Button>
+            ):(
+                <>
+                <Button onClick={()=>{
+                    setDisplay('hide')
+                }} w='100%' pos='static'  bg='RGB(32, 33, 36)' color='white' justifyContent='space-between' _hover={{bg:'RGB(32, 33, 36)'}} >
+               {i.btnTxt}
+               <i className="fa-solid fa-angle-up"></i>
+               </Button>
+               <Box ml={4} display='grid' rowGap={5}>
+               <Text>{i.t1}</Text>
+                <Text>{i.t2}</Text>
+                <Text>{i.t3}</Text>
+                <Text>{i.t4}</Text>
+               </Box>
+               </>
+            ))
+          }
+        </Grid>
+        </>
+    )
+}
