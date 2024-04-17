aa9d7ceb9aa76a83aeb33284a9e27d4dd8a4316c
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-17 22:18:01 +0530

    in footer first section updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,7 @@ const First_section_of_footer = ()=>{
 
     return (
         <>
-        <Grid display={['none','none','none','grid']}  h='auto' color='white' fontSize={{base:"7px",sm:'8px',md:'9px',lg:"12px"}} fontWeight='400' rowGap={12} mt={{base:"90px",sm:"50px",md:"80px"}} justifyContent='center'>
+        <Grid p='6px'  h='auto' color='white' fontSize={{base:"1-px",sm:'10px',md:'10px',lg:"12px"}} fontWeight='400' rowGap={12} mt={{base:"90px",sm:"50px",md:"80px"}} justifyContent='center'>
            <Box sx={margin_left}><Text>Calorie Statement</Text></Box>
            <Box sx={margin_left} w={{base:"90%",sm:'90%',md:"90%",lg:"90%"}}><Text>2,000 calories a day is used for general nutrition advice, but calorie needs vary. Additional nutrition information available upon request in-store and on kfc.com. Prices, participation, and product availability may vary.</Text></Box>
            <Box sx={margin_left} w={{base:"90%",sm:'90%',md:"80%",lg:"60%"}}><Text>Pepsi, Pepsi Globe, Mtn Dew, Mtn Dew Sweet Lightning, Starry are registered trademarks of PepsiCo, Inc.
