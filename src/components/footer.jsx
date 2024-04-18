3d2083d4e1ac92a25aeb13a65b2da8f15dd48149
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-18 10:00:03 +0530

    follow us component adde to the foter

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,4 @@
-import {Grid,Text,Box,Button,Image, border, background} from "@chakra-ui/react"
+import {Grid,Text,Box,Button,Image,Flex, border} from "@chakra-ui/react"
 import { useState } from "react"
 import '../App.css'
 import { color } from "framer-motion"
@@ -22,9 +22,12 @@ export function Footer(){
                 <Responsive_footer_for_small_screens key={i.btnTxt} i={i}/> 
             ))
          } {/* Only visible for small and medium screens */}
-         <Grid templateColumns='69% 31%'>
+
+         <Grid templateColumns={['100%',"100%",'100%','76% 23%']}  alignItems='center'>
             <Privacy_terms/>
+            <Follow_us/>
          </Grid>
+
         </Grid>
         </>
     )
@@ -172,6 +175,30 @@ const Privacy_terms =()=>{
           <Text>Accessibility Statement</Text><Box sx={vertica_row}>|</Box>
           <Text>Build: KFC03222024:a7156544</Text>
         </Box>
+        </>
+    )
+}
+
+const Follow_us = ()=>{
+
+    const box_style={
+
+       w:"14%",
+       h:"24px",
+       borderRadius:"50%",
+       display:'flex',
+       justifyContent:'center',
+       alignItems:'center'
+    }
+
+    return (
+        <>
+        <Flex color='white' p={25} gap={5}  w='100%' justify='center' align='center' fontSize={['20px','22px','22px','15px']}>
+          <Text>Follow Us</Text>
+          <Box sx={box_style}><i className="fa-brands fa-instagram"></i></Box>
+          <Box sx={box_style}><i className="fa-brands fa-facebook"></i></Box>
+          <Box sx={box_style}><i className="fa-brands fa-x-twitter"></i></Box>
+        </Flex>
         </>
     )
 }
