c62e523418349b355b8dbd793a55c31a571444f3
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-19 07:51:51 +0530

    state base conditional rendering added to the pickup delivery left section component

--- src/components/pickup-delivery.jsx
+++ src/components/pickup-delivery.jsx
@@ -1,4 +1,6 @@
-import { Flex,Box,Grid,Text,Button,Input } from "@chakra-ui/react"
+import { useState } from "react"
+
+import { Flex,Box,Grid,Text,Button,Input, border, useStatStyles } from "@chakra-ui/react"
 
 export const Pickup_delivery = ()=>{
 return(
@@ -23,6 +25,8 @@ return(
 
 const Left_section = ()=>{
 
+   const[divBorder,setDivBorder]=useState('pickup')
+
    let button_style={
       bg:'rgb(255, 251, 245)',
       hover:{bg:'rgb(255, 251, 245)'},
@@ -36,15 +40,23 @@ const common_bg={
    bg:'rgb(255, 251, 245)'
 }   
 
+
+
    return(
       <>
        <Grid  borderBottomLeftRadius={25} sx={common_bg} pb={15} placeItems='center' rowGap={4}>
 
          <Text fontSize={22}  fontWeight='700'>How would you like to order?</Text>
+
           <Flex w='100%' bg='red' h='7vh'>
-          <Button sx={button_style} _hover={button_style.hover}>Pick-Up</Button>
-          <Button sx={button_style} _hover={button_style.hover}>Delivery</Button>
+          <Button color={(divBorder=='pickup'?'black':'RGB(101, 101, 101)')} sx={button_style} _hover={button_style.hover} onClick={()=>{setDivBorder('pickup')}}>Pick-Up</Button>
+          <Button color={(divBorder=='delivery'?'black':'RGB(101, 101, 101)')}  sx={button_style} _hover={button_style.hover} onClick={()=>{setDivBorder('delivery')}}>Delivery</Button>
           </Flex>
+
+          <Flex w='100%'>
+             <Box w='50%' border={(divBorder=='pickup'?'1px':'0px')} borderColor={(divBorder=='pickup'?'red':'white')}></Box>
+             <Box w='50%' border={(divBorder=='delivery'?'1px':'0px')} borderColor={(divBorder=='delivery'?'red':'white')}></Box>
+          </Flex> {/** bottom border after pick up and delivery button */}
           
            <Flex w='90%' h='6vh' align='center' margin='auto' border='1px' borderColor='black' mt='25px' borderRadius={15} justify='space-evenly'>
            <i className="fa-solid fa-magnifying-glass"></i>
