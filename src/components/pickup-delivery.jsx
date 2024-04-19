1c33162fc6c2c6892c270599e10270a7aa4c22ea
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-19 07:36:35 +0530

    code updated

--- src/components/pickup-delivery.jsx
+++ src/components/pickup-delivery.jsx
@@ -3,7 +3,7 @@ import { Flex,Box,Grid,Text,Button,Input } from "@chakra-ui/react"
 export const Pickup_delivery = ()=>{
 return(
     <>
-    <Grid pos='absolute' rowGap={4} pos='fixed' m={['0%','0%','0%','11%']} bg='white' w={['100%','100%','100%','80%']}   borderRadius='25px'>
+    <Grid pos='absolute' rowGap={2} pos='fixed' m={['0%','0%','0%','11%']} bg='white' w={['100%','100%','100%','80%']}   borderRadius='25px'>
   
   <Flex  w='100%' h='9vh' borderTopLeftRadius={25} borderTopRightRadius={25} boxShadow='rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;'>
   <Flex w='95%' justify='center' align='center' fontSize={18} fontWeight='700'><Text>START YOUR ORDER</Text></Flex>
@@ -22,19 +22,33 @@ return(
 }
 
 const Left_section = ()=>{
+
+   let button_style={
+      bg:'rgb(255, 251, 245)',
+      hover:{bg:'rgb(255, 251, 245)'},
+      borderRadius:"none",
+      w:'50%',
+      h:'100%',
+      fontWeight:'700'
+   }
+
+const common_bg={
+   bg:'rgb(255, 251, 245)'
+}   
+
    return(
       <>
-       <Grid  borderBottomLeftRadius={25} bg='rgb(255, 251, 245)' pb={15} placeItems='center' rowGap={4}>
+       <Grid  borderBottomLeftRadius={25} sx={common_bg} pb={15} placeItems='center' rowGap={4}>
 
          <Text fontSize={22}  fontWeight='700'>How would you like to order?</Text>
           <Flex w='100%' bg='red' h='7vh'>
-          <Button w='50%' h='100%' borderRadius='none' fontWeight='700'>Pick-Up</Button>
-          <Button w='50%' h='100%' borderRadius='none' fontWeight='700'>Delivery</Button>
+          <Button sx={button_style} _hover={button_style.hover}>Pick-Up</Button>
+          <Button sx={button_style} _hover={button_style.hover}>Delivery</Button>
           </Flex>
           
            <Flex w='90%' h='6vh' align='center' margin='auto' border='1px' borderColor='black' mt='25px' borderRadius={15} justify='space-evenly'>
            <i className="fa-solid fa-magnifying-glass"></i>
-           <input style={{width:'90%', outline:'none',backgroundColor:'rgb(255, 251, 245)'}} type='' placeholder='Search by State,City or zip'/>
+           <input style={{width:'90%', outline:'none',backgroundColor:common_bg.bg}} type='' placeholder='Search by State,City or zip'/>
            </Flex>
             
             <Grid placeItems='center' rowGap={2}>
