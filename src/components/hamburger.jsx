d5eb40b4b6b7fe1b7203043d0166561d32492637
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-23 20:47:53 +0530

    hamburger style completed

--- src/components/hamburger.jsx
+++ src/components/hamburger.jsx
@@ -10,7 +10,7 @@ export const Hamburger = ()=>{
 
     const hamburger_texts_images=[
         {id:1,text:'Menu',image:'https://www.kfc.com/_next/static/images/menu-50641b4da7780fdd2ef91692b9da429c.png'},
-        {id:2,text:'REWARDS',image:"https://www.kfc.com/_next/static/images/Rewards%20EC%201-c467352ec3a159ebe264cb22eac78f5d.svg"},
+        {id:2,text:'REWARDS',image:'https://i.ibb.co/Gp7brYX/Screenshot-2024-04-23-203810.png'},
         {id:3,text:"CAREERS",image:"https://www.kfc.com/_next/static/images/careers-4d39d17a17ca3e563a7e6292790127c5.png"},
         {id:4,text:"MERCH",image:"https://www.kfc.com/_next/static/images/merch-aa94dddedc6eaaf351510faf1cd79194.png"},
         {id:5,text:"FIND A KFC",image:"https://www.kfc.com/_next/static/images/stores-f08efa544c7ec3e36559b83587e198ca.png"},
@@ -18,20 +18,22 @@ export const Hamburger = ()=>{
 
     return(
         <>
-        <Grid   top={130} w='100%' h='auto' bg='white' pos='absolute' justifyContent='center' templateColumns='90%'templateRows='repeat(6,35vh)'  rowGap={10}>
+        <Grid display={['grid','grid','grid','none']}  top={[159,180]} w='100%' h='auto' bg='white' pos='absolute' justifyContent='center' templateColumns='90%'templateRows='repeat(6,auto)'  rowGap={10}>
+
           <Grid color='black'>
               <Text fontSize={50} fontWeight='700' color='RGB(32, 33, 36)'>LET'S GET COOKIN'</Text>
-              <Button bg='RGB(32, 33, 36)' color='white' borderRadius={8} _hover={{bg:"RGB(32, 33, 36)"}} w='14%' fontSize={13} fontWeight='500'>Sign In / Sign Up {'->'}</Button>
+              <Button bg='RGB(32, 33, 36)' color='white' borderRadius={8} _hover={{bg:"RGB(32, 33, 36)"}} w={['40%','26%','20%']} fontSize={13} fontWeight='500'>Sign In / Sign Up {'->'}</Button>
           </Grid>
 
-           <Flex sx={flex_style}>
-            <Text>Menu</Text>
-           </Flex>
-           <Flex sx={flex_style}></Flex>
-           <Flex sx={flex_style}></Flex>
-           <Flex sx={flex_style}></Flex>
-           <Flex sx={flex_style}></Flex>
-
+          {
+            hamburger_texts_images.map(i=>(
+                <Flex key={i.id} sx={flex_style} placeItems='center' justify='space-between' pl={10}>
+                <Text fontWeight='700'>{i.text}</Text>
+                <Image w={['46%','40%','35%']} src={i.image}/>
+               </Flex>
+            ))
+          }
+          
         </Grid>
         </>
     )
