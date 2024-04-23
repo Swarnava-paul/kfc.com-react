bc52edb661750e758967045e5444fa3e978dc284
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-23 21:22:26 +0530

    hamburger mergin from top updated

--- src/components/hamburger.jsx
+++ src/components/hamburger.jsx
@@ -25,7 +25,7 @@ export const Hamburger = ()=>{
 
     return(
         <>
-        <Grid display={[`${hamburgerDisplay}`,`${hamburgerDisplay}`,`${hamburgerDisplay}`,'none']}  top={[159,180]} w='100%' h='auto' bg='white' pos='absolute' justifyContent='center' templateColumns='90%'templateRows='repeat(6,auto)'  rowGap={10}>
+        <Grid display={[`${hamburgerDisplay}`,`${hamburgerDisplay}`,`${hamburgerDisplay}`,'none']}  top={[110,120,137]} w='100%' h='auto' bg='white' pos='absolute' justifyContent='center' templateColumns='90%'templateRows='repeat(6,auto)'  rowGap={10}>
 
           <Grid color='black'>
               <Text fontSize={50} fontWeight='700' color='RGB(32, 33, 36)'>LET'S GET COOKIN'</Text>
