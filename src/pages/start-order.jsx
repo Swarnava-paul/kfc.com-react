ec35ff385338a75561197c30ff61b49f9ebbdae9
Swarnava-paul <swarnavapaul108@gmail.com>
2024-04-24 09:43:27 +0530

    start order page creation process on process and route is also setuped for stat order journey

---
+++ src/pages/start-order.jsx
@@ -0,0 +1,11 @@
+
+
+export const StartOrder = ()=>{
+
+return(
+    <>
+    <h1>strt order page</h1>
+    </>
+)
+
+}
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,10 @@
 import { Routes,Route } from "react-router-dom"
+
+//pages
 import { Homapage } from "../pages/homepage"
 import { Categoriespage } from "../pages/categories"
+import { StartOrder } from "../pages/start-order"
+
 export const Allroutes = ()=>{
 
     return(
@@ -8,6 +12,7 @@ export const Allroutes = ()=>{
      <Routes>
         <Route path='/' element={<Homapage/>}/>
         <Route path="/categories" element={<Categoriespage/>}/>
+        <Route path='/order' element={<StartOrder/>}/>
      </Routes>
 
     </>
